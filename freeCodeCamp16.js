10f5974887e36c26eaf5b3ccfe73eea2dcdf493f
didolaneca@yahoo.com <didolaneca@yahoo.com>
2018-12-18 19:41:00 -0600

    React: Give Sibling Elements a Unique Key Attribute

---
+++ freeCodeCamp16.js
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map((obj) => {
+      return <li key={frontEndFrameworks.indexOf(obj)}>{obj}</li>;
+    });
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
